b1e74e1783a3cd46887c9438a0b2ae1b5221a3a1
Tam Jankowska <tam.jankowska@gmail.com>
2020-10-27 16:36:28 +0000

    Created a basic structure with css
    Imported component files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
-
+import React from 'react';
 import './App.css';
 
+import Navbar from './components/Navbar';
+import Feed from './components/Feed';
+import Trends from './components/Trends';
+
+const App = () => {
+
+
+  return (
+    <div className = "app">
+    
+
+      <div className = "wrapper">
+
+        <div className = "navbar">
+          <p>CONTENT HERE</p>
+          {/* <Navbar /> */}
+        </div>
+
+        <div className = "feed">
+          <p>CONTENT HERE</p>
+          {/* <Feed /> */}
+        </div>
+
+        <div className = "trends">
+          <p>CONTENT HERE</p>
+          {/* <Trends /> */}
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
 
 export default App;
