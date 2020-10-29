8713e454d9ddf67b8744e2f331491733d9bdf635
Tam Jankowska <tam.jankowska@gmail.com>
2020-10-29 14:31:49 +0000

    #13 Added basic tweet function to feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,82 +1,81 @@
-import React, {Component, useState} from 'react';
+import React, {useState} from 'react';
 import './Feed.css';
 import Zardoz from './../images/feed-zardoz.jpg';
 
 const Feed = () => {
     const [tweets, setTweets] = useState([])
-    const [tweet, setTweet] = useState({})
+    const [tweet, setTweet] = useState({
+        user: "Zardoz", 
+        handle: "zarz",
+        profilePic: Zardoz,
+        content: "",
+    })
 
-
-    recordTweet = (event) => {        
-        tweet.user = "Zardoz"
-        tweet.handle = "zarz"
-        tweet.profilePic = Zardoz;
-        tweet.content = event.target.textContent;
-        // update the tweet object to have some sort of post content and 
-        // that's going to come from the event target
-
-        setTweets({
-            currentTweet: tweet
-        })
+    const recordTweet = (event) => {  
+        setTweet({
+            ...tweet,
+            content: event.target.value})
     }
 
-    tweetButton = () => {
-        this.setState({
-            tweets: [...this.state.tweets, this.state.currentTweet],
-            currentTweet: "",
-        })
+    const addTweet = () => {
+        setTweets([
+            ...tweets,
+            {
+                ...tweet,
+                id: tweets.length
+            } 
+        ])
+        console.log(tweets, tweet)
     }
+
+    return (
+        <div className = "feed">
+
+            <div id = "home">
+                <h1 id = "homeLink">Home</h1>
+            </div>
+
+            <div id = "post">
+                <img src = "" alt = ""/>
+                <input 
+                    type = "text"
+                    placeholder = "What's happening?"
+                    onChange = {recordTweet}
+                />
+            </div>
+            <div id = "tweetBar">
+                <button id = "tweet" onClick = {addTweet}>Tweet</button>
+            </div>
+
+            <div id = "twitterFeed">
+                    {tweets.map(tweet => (
+                        <div key = {tweet.id} className = "tweet">
+                            <h2 className = "user">{tweet.user}</h2>
+                            <h3 className = "handle">{tweet.handle}</h3>
+                            <h4 className = "timePosted">Time</h4>
+                            <h5 className = "headline">{tweet.content}</h5>
+                            <img src = {Zardoz}/>
+                        </div>
+                    ))}
+
+
+                    {/* buttons need to be added here  */}
+
+
+
+            </div>
+
+        </div>
+    )
 }
-
-// class Feed2 extends Component {
-
-//     state = {
-//         tweets: []
-//     }
-
-   
-//     render(){
-//         let eachTweet = null;
+    // tweetButton = () => {
+    //     this.setState({
+    //         tweets: [...this.state.tweets, this.state.currentTweet],
+    //         currentTweet: "",
+    //     })
+    // }
 
 
-
-//         return (
-//             <div className = "feed">
-
-//                 <div id = "home">
-//                     <h1 id = "homeLink">Home</h1>
-//                 </div>
-
-//                 <div id = "post">
-//                     <img src = "" alt = ""/>
-//                     <input 
-//                         placeholder = "What's happening?"
-//                     />
-//                 </div>
-//                 <div id = "tweetBar">
-//                     <button id = "tweet">Tweet</button>
-//                 </div>
-
-//                 <div id = "twitterFeed">
-
-//                     <div className = "tweet">
-
-//                         <h2 className = "user">User</h2>
-//                         <h3 className = "handle">@handle</h3>
-//                         <h4 className = "timePosted">Time</h4>
-//                         <h5 className = "headline">Headline</h5>
-//                         <img src = "" alt = "" /> 
-
-//                         {/* buttons need to be added here  */}
-
-//                     </div>
-
-//                 </div>
-
-//             </div>
-//         )
-//     }
-// }
 
 
 
