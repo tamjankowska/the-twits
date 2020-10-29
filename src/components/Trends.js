be0a6d4db37abdb5a38afe6d60d3071334a7687f
Joe Bentley <joe.bentley.20@gmail.com>
2020-10-29 17:02:35 +0000

    #24 Finished JSX + CSS Trends
    Also started media query

--- src/components/Trends.js
+++ src/components/Trends.js
@@ -10,17 +10,16 @@ const Trends = () => {
     return(
 
     <div className="trends-panel">
-        {/* <div className="searchBar" > */}
-            <i class={BiSearch}></i>
+        
+            <i className={BiSearch}>
             <input className="trends-searchBar" type="text"  placeholder="Search Twitter" ></input>
-            {/* </div> */}
+            </i>
             <aside className="trends-aside">
-                <h2>What's Happening</h2>
+                <h2>What's happening</h2>
                 <article className="trend-article">
                     <p>US elections. 28 minutes ago</p>
-                    <h3>Biden seeks to poach votes in Republican South
-                        <img className="trends-image" src={Biden} alt="Joe Biden" />
-                    </h3>
+                    <img className="trends-image" src={Biden} alt="Joe Biden" />
+                    <h3>Biden seeks to poach votes in Republican South</h3> 
                     <p>Trending with  <a target="/" href="https://www.bbc.co.uk/news/live/election-us-2020-54704747"> BBC News</a></p>
                 </article>
                 <article className="trend-article">
@@ -30,9 +29,8 @@ const Trends = () => {
                 </article>
                 <article className="trend-article">
                     <p>Trending in United Kingdom</p>
-                    <h3>Free School meals for kids
-                        <img className="trends-image" src={TrendsFood} alt="Food" />
-                    </h3>
+                    <img className="trends-image" src={TrendsFood} alt="Food" />
+                    <h3>Free School meals for kids</h3>
                     <p>Trending with <a target="/" href="https://www.bbc.co.uk/news/uk-54669929"> Marcus Rashford</a></p>
                 </article>
                 <article className="trend-article">
@@ -47,6 +45,14 @@ const Trends = () => {
                     <a href="/" target="/">Show more</a>
                 </div>
             </aside>
+            <footer className="trends-footer" > 
+                <a href="/" target="/">Terms of Service</a>
+                <a href="/" target="/">Privacy Policy</a>
+                <a href="/" target="/">Cookie Policy</a>
+                <a href="/" target="/">Ads info</a>
+                <a href="/" target="/">More...</a>
+                <a href="/" target="/">&copy;2020 Twitter, Inc</a>
+            </footer>
        
     </div>        
         
